5f8dd0861ba5b60dbebcb5a9825543aca191bf7c
Thiago Cordeiro <thiagocordeirodantasw@gmail.com>
2023-04-03 10:50:18 -0300

    lidando com promises

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,49 +1,54 @@
-const http = new XMLHttpRequest()
-
-http.open('GET','http://localhost:3000/profile')
-
-http.send()
-
-http.onload = () => {
-    const data = JSON.parse(http.response)
-    data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))
-    })
-
-    const http2 = new XMLHttpRequest()
-    http2.open('GET','http//localhost:3000/profile/semanaPassada')
-
-    http2.onload = () => {
-        ..
-        const http3 = new XMLHttpRequest()
-        http3.open('GET','http//localhost:3000/profile/semanaRetrasada')
-
-        http3.onload = () => {
-            ..
-        }
-
-    }
-    http2.send()
-}
-
 
 const criaNovaLinha = (nome,email)  => {
-     const linhaNovoCliente = document.createElement('tr')
-
-const conteudo = 
-            `<td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>   
-                </td> ` 
-
+    const linhaNovoCliente = document.createElement('tr')
+    
+    const conteudo = 
+    `<td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="tabela__botoes-controle">
+    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>   
+    </td> ` 
+    
     linhaNovoCliente.innerHTML = conteudo
     return linhaNovoCliente
 }
 
 
 const tabela = document.querySelector('[data-tabela]')
+
+
+const listaClientes = () => {
+    const promise = new Promise((resolve, reject) => {
+        const http = new XMLHttpRequest()
+
+        http.open('GET','http://localhost:3000/profile')
+        
+        
+        http.onload = () => {
+                if(http.status >= 400){
+                    reject(JSON.parse(http.response))
+                } else {
+                    resolve(JSON.parse(http.response))
+                }
+        }
+        http.send()
+    })
+    return promise
+}
+        
+    
+    
+
+
+listaClientes()
+.then(  data => {
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))
+    })
+})
+
+
 
