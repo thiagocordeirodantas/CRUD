fe41984827c1107104b56203229938096a99f329
Thiago Cordeiro <thiagocordeirodantasw@gmail.com>
2023-04-03 11:12:35 -0300

    refatorando responsabilidades

---
+++ admin/controller/listaClienteController.js
@@ -0,0 +1,30 @@
+import { ClienteService } from "../service/cliente-service.js"
+
+const tabela = document.querySelector('[data-tabela]')
+
+const criaNovaLinha = (nome,email)  => {
+    const linhaNovoCliente = document.createElement('tr')
+    
+    const conteudo = 
+    `<td class="td" data-td>${nome}</td>
+    <td>${email}</td>
+    <td>
+    <ul class="tabela__botoes-controle">
+    <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+    <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+    </ul>   
+    </td> ` 
+    
+    linhaNovoCliente.innerHTML = conteudo
+    return linhaNovoCliente
+}
+
+ClienteService.listaClientes()
+.then(  data => {
+    data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email))
+    })
+})
+
+
+
