44ce1d9a0da45db69060aa2ec77d98394990894a
Thiago Cordeiro <thiagocordeirodantasw@gmail.com>
2023-04-03 10:55:48 -0300

    rescrevendo com fetch API

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -21,22 +21,10 @@ const tabela = document.querySelector('[data-tabela]')
 
 
 const listaClientes = () => {
-    const promise = new Promise((resolve, reject) => {
-        const http = new XMLHttpRequest()
-
-        http.open('GET','http://localhost:3000/profile')
-        
-        
-        http.onload = () => {
-                if(http.status >= 400){
-                    reject(JSON.parse(http.response))
-                } else {
-                    resolve(JSON.parse(http.response))
-                }
-        }
-        http.send()
+    return fetch('http://localhost:3000/profile')
+    .then(resposta => {
+        return resposta.json()
     })
-    return promise
 }
         
     
