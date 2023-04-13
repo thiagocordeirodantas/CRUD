1cd02d2e5a78943934b245099689d4c95ac7499d
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-13 14:48:59 -0300

    async await projeto final

--- admin/controller/listaClientes-controller.js
+++ admin/controller/listaClientes-controller.js
@@ -7,7 +7,7 @@ const criaNovaLinha = (nome, email, id) =>  {
                   <td>${email}</td>
                   <td>
                       <ul class="tabela__botoes-controle">
-                          <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
+                          <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
                           <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
                       </ul>
                   </td> 
@@ -20,20 +20,35 @@ const criaNovaLinha = (nome, email, id) =>  {
 
 const tabela = document.querySelector('[data-tabela]')
 
-tabela.addEventListener('click', (evento)=> {
+tabela.addEventListener('click', async (evento)=> {
     let ehBotaoDeDeleta = evento.target.className === 'botao-simples botao-simples--excluir'
     if(ehBotaoDeDeleta){
-        const linhaCliente = evento.target.closest('[data-id]')
-        let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-        .then(()=>{
+        try {
+            const linhaCliente = evento.target.closest('[data-id]')
+            let id = linhaCliente.dataset.id
+            await clienteService.removeCliente(id)
             linhaCliente.remove()
-        })
+        }
+        catch(erro){
+            console.log(erro)
+            window.location.href="../telas/erro.html"
+        }
     }
 })
 
-clienteService.listaClientes()
-.then(data => {
-        data.forEach(elemento => {
-        tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email, elemento.id))
-})})
+
+const render = async () =>  {
+    try {
+        const listaClientes = await clienteService.listaClientes()
+        listaClientes.forEach(elemento => {
+            tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email, elemento.id))
+    })
+    }
+    catch(erro){
+        console.log(erro)
+        window.location.href="../telas/erro.html"
+    }
+    
+}
+
+render()
