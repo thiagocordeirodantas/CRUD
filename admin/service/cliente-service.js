ee0f1e88a14cacca971352f181b000ad4d7c6c83
Thiago Cordeiro Dantas <thiiaguinhocordeirodantas@gmail.com>
2023-04-13 14:48:00 -0300

    Editando dados

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,27 +1,34 @@
-export const listaClientes  = () => {
-    return fetch('http://localhost:3000/profile')
+const listaClientes = () =>  {
+    return fetch(`http://localhost:3000/profile`)
     .then(resposta => {
         return resposta.json()
     })
 }
 
-const criaCliente = (nome,email) => {
-    return fetch('http://localhost:3000/profile', {
-        method: 'POST',
+const criaCliente = (nome, email) => { 
+    return fetch(`http://localhost:3000/profile`, {
+        method: 'POST', 
         headers: {
-            "Content-Type" : "application/json"
+            'Content-Type' : 'application/json'
         },
         body: JSON.stringify({
             nome: nome,
             email: email
         })
-    }) 
+    })
     .then( resposta => {
-       return resposta.body
+        return resposta.body
     })
 }
 
-export const ClienteService = {
+const removeCliente = (id) => { 
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    })
+}
+
+export const clienteService = { 
     listaClientes,
-    criaCliente
+    criaCliente, 
+    removeCliente
 }
